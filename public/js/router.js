6ca7b5bf8c305647ddf31eae09112865557f9ed2
William Humphreys-Cloutier <williamhumphreyscloutier@gmail.com>
2014-02-27 15:11:50 -0600

    basic ember app configuration

--- public/js/router.js
+++ public/js/router.js
@@ -1,9 +1,9 @@
 (function () {
   App.Router.map(function () {
     // for the future, when we have more controllers
-    // this.resource('trips', { path: '/' }, function () {
-    //   this.route('new');
-    // });
+    this.resource('trips', { path: '/' }, function () {
+      this.route('new');
+    });
   });
 
   App.TripsRoute = Ember.Route.extend({
