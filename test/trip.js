470f7b0fbd25451c56113958eea6f5db62a0a2a2
umcook73 <umcook73@gmail.com>
2014-02-28 12:13:57 -0600

    Added badid test
    Got the badid test to function, the new way of posting trips seems to be
    failing.

--- test/trip.js
+++ test/trip.js
@@ -5,15 +5,21 @@ var app = require('../app.js');
 var request = require('supertest');
 
 describe('GET /trips/:badid', function(){
-	it('should return json with a 500 error');
+
+	it('should return json with a 500 error', function(done){
+		request(app)
+			.get('/trips/:badid')
+			.expect('Content-Type', /json/)
+			.expect(500, done);
+	})
 })
 
-describe('POST /trips/create', function(){
-	
+describe('POST /trips', function(){	
+
 	it('should respond with json', function(done){
 		request(app)
-			.post('/trips/create')
-			.send({tripname: "TESTTRIP"})
+			.post('/trips')
+			.send({"Trip":{"test": "TESTTRIP"}})
 			.set('Accept','application/json')
 			.expect('Content-Type', /json/)
 			.expect(200, done);
