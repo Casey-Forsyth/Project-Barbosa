8965d92b5d6b657c33ecc3f2c76cd30e1e5317a4
William Humphreys-Cloutier <williamhumphreyscloutier@gmail.com>
2014-03-06 18:56:53 -0600

    added top-level describe block for home controller tests

--- test/app.js
+++ test/app.js
@@ -4,20 +4,22 @@ var express = require('express');
 var app = require('../app.js');
 var request = require('supertest');
 
-describe('GET /', function() {
-  it('should be return html', function(done) {
-    request(app)
-      .get('/')
-      .expect('Content-Type', /html/)
-      .expect(200, done);
+describe('Home Controller', function(){
+  describe('GET /', function() {
+    it('should return html', function(done) {
+      request(app)
+        .get('/')
+        .expect('Content-Type', /html/)
+        .expect(200, done)
+    })
   })
-})
 
-describe('GET /NOTHINGHERE', function() {
-  it('should return 404', function(done) {
-    request(app)
-      .get('/NOTHINGHERE')
-      .expect('Content-Type', /html/)
-      .expect(404, done);
+  describe('GET /not_an_api_endpoint', function() {
+    it('should return 404', function(done) {
+      request(app)
+        .get('/not_an_api_endpoint')
+        .expect('Content-Type', /html/)
+        .expect(404, done)
+    })
   })
 })
