c7fc8b27bade98c04cd5ac15b391f0d4e3b4a5f1
William Humphreys-Cloutier <williamhumphreyscloutier@gmail.com>
2014-03-08 13:09:06 -0600

    syntax fix - JS objects need commas

--- models/Trip.js
+++ models/Trip.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var ItineraryItem = require('./ItineraryItem');
 
 var tripSchema = new mongoose.Schema({
-  name:  {type: String, default: "My Trip"}
-  itinerary: [ItineraryItem.schema]
+  name:  {type: String, default: "My Trip"},
+  itinerary: [ItineraryItem.schema],
   archived: {type: Boolean, default: false}
 });
 
