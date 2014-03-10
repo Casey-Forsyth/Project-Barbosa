45796601cf5ed4779c1fd53905b568d714dd82f5
Barret Monchka <barret.monchka@gmail.com>
2014-03-10 16:35:58 -0500

    Added basic object tests for itinerary items.

--- test/trip_itinerary.js
+++ test/trip_itinerary.js
@@ -25,6 +25,45 @@ describe('Itinerary Items', function() {
   itineraryItem2Title = "Test Item 2";
   itineraryItem2 = new ItineraryItem({title: itineraryItem2Title});
 
+  describe('Object Properties', function() {
+
+    it('should have a name', function(done) {
+      item_name = "trip item name"
+      itineraryItem = new ItineraryItem();
+      itineraryItem.name = item_name;
+      assert.equal(itineraryItem.name, item_name)
+      done()
+    })
+
+    it('should have a updatedAt Date, defaulting to now', function(done) {
+      now = new Date()
+      itineraryItem = new ItineraryItem()
+      assert.equal(itineraryItem.updated.toISOString(), now.toISOString())
+      done()
+    })
+
+    it('should have a scheduledAt Date, defaulting to null', function(done) {
+      now = new Date()
+      itineraryItem = new ItineraryItem()
+      assert.equal(itineraryItem.updatedAt, null)
+      itineraryItem.updatedAt = now
+      assert.equal(itineraryItem.updated.toISOString(), now.toISOString())
+      done()
+    })
+
+    it('should have a location with a name, lat, and long', function(done) {
+      loc = {
+        name: "Takeoff from YWG",
+        latitude: 49.89,
+        longitude: -97.1,
+      }
+      itineraryItem = new ItineraryItem({location: loc})
+      itineraryItem.location.should.have.properties(['name', 'latitude', 'longitude'])
+      done()
+    })
+
+  })
+
   describe('GET /trip/:tripid/items', function() {
 
     it('should respond with an empty list of itinerary items', function(done) {
