1d3718a83dce19ef34ad6243a331fbd44b6158c5
Barret Monchka <barret.monchka@gmail.com>
2014-03-10 15:16:12 -0500

    Added tests for returning a single itinerary item

--- test/trip_itinerary.js
+++ test/trip_itinerary.js
@@ -19,20 +19,22 @@ before(function() {
 
 describe('Itinerary Items', function() {
 
+  itineraryItem1Title = "Test Item 1";
+  itineraryItem1 = new ItineraryItem({title: itineraryItem1Title});
+
+  itineraryItem2Title = "Test Item 2";
+  itineraryItem2 = new ItineraryItem({title: itineraryItem2Title});
+
   describe('GET /trip/:tripid/items', function() {
 
     it('should respond with an empty list of itinerary items', function(done) {
 
-      Trip.find().exec(function(err, items){
-
-        request(app)
-          .get('/trip/' + testTrip.id + '/items')
-          .set('Accept','application/json')
-          .expect('Content-Type', /json/)
-          .expect(200, {itinerary: []})
+      request(app)
+        .get('/trip/' + testTrip.id + '/items')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, {itinerary: []})
         .end(done)
-
-      });
 
     });
 
@@ -40,18 +42,12 @@ describe('Itinerary Items', function() {
 
 	describe('POST /trips/:tripid/items', function() {
 
-		itineraryItem1Title = "Test Item 1";
-		itineraryItem1 = new ItineraryItem({title: itineraryItem1Title});
-
-		itineraryItem2Title = "Test Item 1";
-		itineraryItem2 = new ItineraryItem({title: itineraryItem1Title});
-
 	  it('should add a new itinerary item (first item)', function(done) {
 
       request(app)
         .post('/trips/' + testTrip.id + '/items')
         .send({itineraryItem: itineraryItem1})
-        .set('Accept','application/json')
+        .set('Accept', 'application/json')
         .end(function(err, res){
           res.should.have.status(200)
           res.body.trip.itinerary.should.have.length(1)
@@ -70,7 +66,7 @@ describe('Itinerary Items', function() {
       request(app)
         .post('/trips/' + testTrip.id + '/items')
         .send({itineraryItem: itineraryItem2})
-        .set('Accept','application/json')
+        .set('Accept', 'application/json')
         .end(function(err, res){
           res.should.have.status(200)
           res.body.trip.itinerary.should.have.length(2)
@@ -90,20 +86,60 @@ describe('Itinerary Items', function() {
 
     it('should respond with a non-empty list of itinerary items (length of 2)', function(done) {
 
-      Trip.find().exec(function(err, items){
+      request(app)
+        .get('/trip/' + testTrip.id + '/items')
+        .set('Accept', 'application/json')
+        .end(function(err, res){
+          res.should.have.status(200)
+          res.body.itinerary.should.have.length(2)
+          res.body.itinerary[0].should.have.property('title', itineraryItem1Title)
+          res.body.itinerary[1].should.have.property('title', itineraryItem2Title)
+          done()
+        });
 
-        request(app)
-          .get('/trip/' + testTrip.id + '/items')
-          .set('Accept','application/json')
-          .end(function(err, res){
-            res.should.have.status(200)
-            res.body.itinerary.should.have.length(2)
-            res.body.itinerary[0].should.have.property('title', itineraryItem1Title)
-            res.body.itinerary[1].should.have.property('title', itineraryItem2Title)
-            done()
-          });
+    });
 
-      });
+  });
+
+  describe('GET /trip/:tripid/items/:itemid', function() {
+
+    it('should respond with null (invalid itinerary item id)', function(done) {
+
+      request(app)
+        .get('/trip/' + testTrip.id + '/items/' + 'invalid_itinerary_item_id')
+        .set('Accept', 'application/json')
+        .expect(500, {})
+        .end(done)
+
+    });
+
+    it('should respond with a single itinerary items (first item)', function(done) {
+
+      request(app)
+        .get('/trip/' + testTrip.id + '/items/' + itineraryItem1.id)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .end(function(err, res){
+          res.should.have.status(200)
+          res.body.itineraryItem.should.have.property('_id', itineraryItem1.id)
+          res.body.itineraryItem.should.have.property('title', itineraryItem1.title)
+          done()
+        });
+
+    });
+
+    it('should respond with a single itinerary items (second item)', function(done) {
+
+      request(app)
+        .get('/trip/' + testTrip.id + '/items/' + itineraryItem2.id)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .end(function(err, res){
+          res.should.have.status(200)
+          res.body.itineraryItem.should.have.property('_id', itineraryItem2.id)
+          res.body.itineraryItem.should.have.property('title', itineraryItem2.title)
+          done()
+        });
 
     });
 
