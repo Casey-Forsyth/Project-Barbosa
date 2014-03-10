4ee62b8166564f9a2b9b8ca6a8831407e91e7991
Barret Monchka <barret.monchka@gmail.com>
2014-03-10 14:57:20 -0500

    Added tests for returning a list of itinerary items and creating itinerary items.

--- test/trip_itinerary.js
+++ test/trip_itinerary.js
@@ -7,89 +7,110 @@ var Trip = require('../models/Trip');
 var app = require('../app.js');
 var request = require('supertest');
 
-describe('Trip items', function() {
-  it('should have a name', function(done) {
-    item_name = "trip item name"
-    itineraryItem = new ItineraryItem();
-    itineraryItem.name = item_name;
-    assert.equal(itineraryItem.name, item_name)
-    done()
-  })
+var testTrip = new Trip({name: "*Test Trip*"});
 
-  it('should have a updatedAt Date, defaulting to now', function(done) {
-    now = new Date()
-    itineraryItem = new ItineraryItem()
-    assert.equal(itineraryItem.updated.toISOString(), now.toISOString())
-    done()
-  })
-
-  it('should have a scheduledAt Date, defaulting to null', function(done) {
-    now = new Date()
-    itineraryItem = new ItineraryItem()
-    assert.equal(itineraryItem.updatedAt, null)
-    itineraryItem.updatedAt = now
-    assert.equal(itineraryItem.updated.toISOString(), now.toISOString())
-    done()
-  })
-
-  it('should have a location with a name, lat, and long', function(done) {
-    loc = {
-      name: "Takeoff from YWG",
-      latitude: 49.89,
-      longitude: -97.1,
+before(function() {
+  testTrip.save(function(err) {
+    if (err && err.code != 11000) { //Create a test trip. If trip exists (err.code 11000 returned), do nothing.
+      assert.fail(err, null, 'Test trip creation error.')
     }
-    itineraryItem = new ItineraryItem({location: loc})
-    itineraryItem.location.should.have.properties(['name', 'latitude', 'longitude'])
-    done()
   })
-
 })
 
-describe('POST /trips/:tripid/items', function() {
-  it('should respond with json', function(done) {
+describe('Itinerary Items', function() {
 
-    itineraryItem = new ItineraryItem();
-    itineraryItem.name = "Test Item";
+  describe('GET /trip/:tripid/items', function() {
 
-    Trip.find().exec(function(err, trips){
+    it('should respond with an empty list of itinerary items', function(done) {
 
-		var trip = trips[0];
-		request(app)
-		  .post('/trips/' + trip.id + '/items')
-		  .send({itineraryItem:itineraryItem})
-		  .set('Accept','application/json')
-		  .expect('Content-Type', /json/)
-		  .expect(200, done);
-	});
+      Trip.find().exec(function(err, items){
+
+        request(app)
+          .get('/trip/' + testTrip.id + '/items')
+          .set('Accept','application/json')
+          .expect('Content-Type', /json/)
+          .expect(200, {itinerary: []})
+        .end(done)
+
+      });
+
+    });
+
+  });
+
+	describe('POST /trips/:tripid/items', function() {
+
+		itineraryItem1Title = "Test Item 1";
+		itineraryItem1 = new ItineraryItem({title: itineraryItem1Title});
+
+		itineraryItem2Title = "Test Item 1";
+		itineraryItem2 = new ItineraryItem({title: itineraryItem1Title});
+
+	  it('should add a new itinerary item (first item)', function(done) {
+
+      request(app)
+        .post('/trips/' + testTrip.id + '/items')
+        .send({itineraryItem: itineraryItem1})
+        .set('Accept','application/json')
+        .end(function(err, res){
+          res.should.have.status(200)
+          res.body.trip.itinerary.should.have.length(1)
+          res.body.trip.itinerary[0].should.have.property('title', itineraryItem1Title)
+          // make sure the item actually exists in the DB
+          Trip.findById(testTrip.id, function(err, trip){
+            trip.itinerary[0].should.have.property('id', itineraryItem1.id)
+          })
+          done()
+        });
+
+		});
+
+    it('should add a new itinerary item (second item)', function(done) {
+
+      request(app)
+        .post('/trips/' + testTrip.id + '/items')
+        .send({itineraryItem: itineraryItem2})
+        .set('Accept','application/json')
+        .end(function(err, res){
+          res.should.have.status(200)
+          res.body.trip.itinerary.should.have.length(2)
+          res.body.trip.itinerary[1].should.have.property('title', itineraryItem2Title)
+          // make sure the item actually exists in the DB
+          Trip.findById(testTrip.id, function(err, trip){
+            trip.itinerary[1].should.have.property('id', itineraryItem2.id)
+          })
+          done()
+        });
+
+    });
+
+  });
+
+  describe('GET /trip/:tripid/items', function() {
+
+    it('should respond with a non-empty list of itinerary items (length of 2)', function(done) {
+
+      Trip.find().exec(function(err, items){
+
+        request(app)
+          .get('/trip/' + testTrip.id + '/items')
+          .set('Accept','application/json')
+          .end(function(err, res){
+            res.should.have.status(200)
+            res.body.itinerary.should.have.length(2)
+            res.body.itinerary[0].should.have.property('title', itineraryItem1Title)
+            res.body.itinerary[1].should.have.property('title', itineraryItem2Title)
+            done()
+          });
+
+      });
+
+    });
 
   });
 
 });
 
-describe('GET /trip/:tripid/items', function() {
-  it('should respond with json', function(done) {
-
-	var tripid;
-
-    itineraryItem = new ItineraryItem();
-    itineraryItem.name = "Test Item";
-
-    Trip.find().exec(function(err, trips){
-
-		var trip = trips[0];
-		tripid = trip.id;
-		request(app)
-		  .post('/trips/' + tripid + '/items')
-		  .send({itineraryItem:itineraryItem})
-
-	});
-
-	request(app)
-	  .get('/trip/' + trip.id + '/items')
-	  .set('Accept','application/json')
-	  .expect('Content-Type', /json/)
-	  .expect(200, done);
-
-  });
-
+after(function() {
+  testTrip.remove()
 })
