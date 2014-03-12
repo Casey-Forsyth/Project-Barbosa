5209e546da2fca8f509dd12931f69621abdcc8f5
William Humphreys-Cloutier <williamhumphreyscloutier@gmail.com>
2014-03-12 18:54:28 -0500

    fix value binding with non-nested location name...
    because ember's stupid

--- models/ItineraryItem.js
+++ models/ItineraryItem.js
@@ -1,14 +1,12 @@
 var mongoose = require('mongoose');
 
 var itineraryItemSchema = new mongoose.Schema({
-  title: {type: String, default: ''},
-  description: {type: String, default: ''},
-  scheduledAt: {type: Date, default: null},
-  location: {
-    name: {type: String, default: ''},
-    latitude: {type: Number, default: 49.89},
-    longitude: {type: Number, default: -97.1},
-  },
+  title:        {type: String, default: ''},
+  description:  {type: String, default: ''},
+  locationName: {type: String, default: ''},
+  scheduledAt:  {type: Date, default: null},
+  latitude:     {type: Number, default: 49.89},
+  longitude:    {type: Number, default: -97.1},
 });
 
 module.exports.model = mongoose.model('ItineraryItem', itineraryItemSchema);
--- public/js/models/item.js
+++ public/js/models/item.js
@@ -1,17 +1,10 @@
 var Item = DS.Model.extend({
-
   title: DS.attr('string'),
-
   description: DS.attr('string'),
-
   scheduledAt: DS.attr('date'),
-
-  location: {
-    name: DS.attr('string'),
-    latitude: DS.attr('number'),
-    longitude: DS.attr('number')
-  }
-
+  locationName: DS.attr('string'),
+  latitude: DS.attr('number'),
+  longitude: DS.attr('number')
 });
 
 module.exports = Item;
