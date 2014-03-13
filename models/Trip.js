cf126bc32fb3ce2733f77ecab56bbfb9c91dda6a
Casey Forsyth <caseyvill@gmail.com>
2014-03-12 22:33:20 -0500

    Added Trip Created Date

--- models/Trip.js
+++ models/Trip.js
@@ -1,9 +1,12 @@
 var mongoose = require('mongoose');
 var ItineraryItem = require('./ItineraryItem');
+var User = require('./User');
 
 var tripSchema = new mongoose.Schema({
   name:  {type: String, default: "My Trip"},
   itinerary: [ItineraryItem.schema],
+  user:[User.Schema],
+  date: {type: Date, default: Date.now()},
   archived: {type: Boolean, default: false}
 });
 
--- public/js/models/trip.js
+++ public/js/models/trip.js
@@ -1,6 +1,7 @@
 var Trip = DS.Model.extend({
 
   "name": DS.attr('string'),
+  "date": DS.attr('date'),
 
   // "_id": DS.attr('string'),
 
@@ -8,3 +9,21 @@ var Trip = DS.Model.extend({
 
 module.exports = Trip;
 
+
+var moment = require('moment');
+moment().format();
+
+Handlebars.registerHelper("formatDate", function(datetime, format) {
+  // if (moment) {
+  //   f = DateFormats[format];
+  //   return moment(datetime).format(f);
+  // }
+  // else {
+    return datetime;
+  // }
+});
+
+var DateFormats = {
+       short: "DD MMMM - YYYY",
+       long: "dddd DD.MM.YYYY HH:mm"
+};
