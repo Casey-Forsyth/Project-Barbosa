3a2889bb7594c062ec4ad45d57f1bb5ea6f51965
Casey Forsyth <caseyvill@gmail.com>
2014-03-13 23:27:06 -0500

    Added tests for Trip controller (tests saving field, if changes are saved, if null is accepted and if the default works).
    Re-arranged tests to match the order of the fields.
    
    James and Casey

--- test/trip.js
+++ test/trip.js
@@ -5,22 +5,109 @@ var request = require('supertest');
 var Trip = require('../models/Trip');
 
 describe('Trip Model', function(){
+
+  //TRIP NAME
   it('should have a name', function(){
     n1 = 'MEXICO!!!'
     n2 = 'CUBA!!!!!'
-    t = new Trip({name: n2})
+    var t = new Trip({name: n2})
     t.should.have.property('name', n2)
     t.name = n2
     t.should.have.property('name', n2)
   })
 
+  it('should save changes to the name flag', function(){
+    var t = new Trip({name: "test"})
+    t.should.have.property('name', "test");
+    t.name = "changed";
+    t.should.have.property('name', "changed")
+  })
+
+  it('should accept null', function(){
+    var t = new Trip({name: null})
+    t.should.have.property('name', null)
+  })
+
   it('should have a default `name` of "My Trip"', function(){
-    t = new Trip()
+   var  t = new Trip()
     t.should.have.property('name', 'My Trip')
   })
 
-  it('should have a default `archived` flag of false', function(){
-    t = new Trip()
+//USER
+  it('should save the user flag', function(){
+    var t = new Trip({user: "test"})
+    t.should.have.property('user', "test");
+  })
+
+  it('should save changes to the user flag', function(){
+    var t = new Trip({user: "test"})
+    t.should.have.property('user', "test");
+    t.user = "changed";
+    t.should.have.property('user', "changed")
+  })
+
+  it('should accept null', function(){
+    var t = new Trip({user: null})
+    t.should.have.property('user', null)
+  })
+
+  it('should have a default `user` flag of `None`', function(){
+   var  t = new Trip()
+    t.should.have.property('user', "None")
+  })
+
+  //LOCATION FLAG
+  it('should save the location flag', function(){
+    var t = new Trip({location: "test"})
+    t.should.have.property('location', "test");
+  })
+
+  it('should save changes to the location flag', function(){
+    var t = new Trip({location: "test"})
+    t.should.have.property('location', "test");
+    t.location = "changed";
+    t.should.have.property('location', "changed")
+  })
+
+  it('should accept null', function(){
+    var t = new Trip({location: null})
+    t.should.have.property('location', null)
+  })
+  it('should have a default `location` flag of `None`', function(){
+   var  t = new Trip()
+    t.should.have.property('location', "None")
+  })
+
+  //DATE FLAG
+  it('should have a default `date` flag of approximately now', function(){
+      var bottomDate = Date.now()-2000;
+      var t = new Trip()
+      var tripdate = Date.parse(t.date);
+      var topDate = Date.now()+2000;
+      tripdate.should.be.within(bottomDate,topDate);
+  })
+
+   //ARCHIVED FLAG
+  it('should save the archived flag', function(){
+    var t = new Trip({archived: true})
+    t.should.have.property('archived', true);
+  })
+
+  it('should save changes to the archived flag', function(){
+    var t = new Trip({archived: true})
+    t.should.have.property('archived', true);
+    t.archived = false;
     t.should.have.property('archived', false)
   })
+
+  it('should accept null', function(){
+    var t = new Trip({archived: null})
+    t.should.have.property('archived', null)
+  })
+
+  it('should have a default `archived` flag of false', function(){
+   var  t = new Trip()
+    t.should.have.property('archived', false)
+  })
+
 })
