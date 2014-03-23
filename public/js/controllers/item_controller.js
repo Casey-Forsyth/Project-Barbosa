00610a6a0ec194a9c9c6c3cb0c62a4e91c88d636
umcook73 <umcook73@gmail.com>
2014-03-23 15:39:09 -0500

    Changes to itinerary item

--- public/js/controllers/item_controller.js
+++ public/js/controllers/item_controller.js
@@ -3,6 +3,7 @@ var ItemController = Ember.ObjectController.extend({
   actions: {
     save: function() {
       this.get('store').commit();
+	  return this.send('closeModal');
     },
     close: function() {
       return this.send('closeModal');
